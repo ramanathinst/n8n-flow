7d2d1db9286f4c1fe99f6354f68c26a7c9ee1475
Ramanath <ramanathinst@gmail.com>
2026-01-11 12:33:34 +0530

    07: error tracking

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,16 +1,25 @@
 import { inngest } from "./client";
 import { openai } from "@ai-sdk/openai";
 import { generateText } from "ai";
+import * as Sentry from "@sentry/nextjs";
+
 
 export const aiExecute = inngest.createFunction(
     { id: "ai-execute" },
     { event: "ai/execute" },
+    async ({ event, step }) => {
 
-    async ({ event, step }) => { 
-        const {steps} = await step.ai.wrap("openapi-generate-text", generateText, {
+        Sentry.logger.info('User triggered ai execute', { log_source: 'sentry_test' })
+
+        const { steps } = await step.ai.wrap("openapi-generate-text", generateText, {
             model: openai("gpt-5"),
             system: "You are helpfull assistent",
-            prompt: "latest new of war?"
+            prompt: "what is 9 + 9?",
+            experimental_telemetry: {
+                recordInputs: true,
+                recordOutputs: true,
+                isEnabled: true
+            }
         })
         return steps;
     }
