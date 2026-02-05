6344acaa5a3ee0d06cbedf38567c07e5b28bbf95
Ramanath <ramanathinst@gmail.com>
2026-02-05 16:26:33 +0530

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Card, CardTitle } from "@/components/ui/card";
+import prisma from "@/lib/db";
 
-const Page = () => {
+const Page = async() => {
+  const users = await prisma.user.findMany({})
   return(
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Card className="px-5 py-5">
         <CardTitle>
           This is n8n-flow
+          {JSON.stringify(users, null, 2)}
         </CardTitle>
       </Card>
     </div>
